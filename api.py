bdace7ac085c4d72da6eece6586179db3f708373
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-11-26 10:55:50 +0530

    26 NOV 2020

--- api.py
+++ api.py
@@ -1,2 +1,22 @@
-print("hello")
 import flask
+from flask import request
+import random
+
+import random
+no = random.randint(0,9)
+
+app = flask.Flask(__name__)
+
+@app.route('/', method=["GET"])
+
+def home():
+    arg = request.arg['pos']
+    while True:
+        no = random.randint(0,9)
+        if no != arg:
+            break
+    
+    try:
+        return f"My Position {no} "
+    except KeyError:
+        return "Invalid input"
